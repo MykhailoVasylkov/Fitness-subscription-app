b151156cc601b115784307f33948f0c2b7961c84
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-04-09 14:05:37 +0100

    Added custom widget for photo input

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
-# Used code from Boutique Ado
+
 
 class ProductForm(forms.ModelForm):
 
@@ -8,7 +9,7 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
