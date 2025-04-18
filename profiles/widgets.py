419f88548b2164bfc2ddde12c34be761a19314e8
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-04-18 23:21:49 +0100

    Added script to handle avatar preview on the client side

--- profiles/widgets.py
+++ profiles/widgets.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 Code snippet fro Boutique Ado
 '''
 class CustomClearableFileInput(ClearableFileInput):
-    clear_checkbox_label = _('Remove')
+    clear_checkbox_label = _('Remove Avatar')
     initial_text = _('Current Profile Image')
     input_text = _('')
     template_name = 'profiles/custom_widget_templates/custom_clearable_file_input.html'
