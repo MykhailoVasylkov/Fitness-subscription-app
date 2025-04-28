8081824f8fb3820fe624193edd4b3e079a6f93e1
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-04-28 14:51:58 +0100

    Prepare project for deployment

--- fithub/settings.py
+++ fithub/settings.py
@@ -24,13 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^t#-b%4osv_fcsvz#om=8rouz9pi^5oql#i(r8gk!z*3w#3_5h'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                 'localhost',]
+                 'localhost',
+                 'fit-hub-project-5fab9d2a0d84.herokuapp.com',]
 
 
 # Application definition
