2ba96ade4753e3fb91b4d38bb8fa81f76545a822
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-04-28 15:35:41 +0100

    Added send_order_status_email signal

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,6 +1,10 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
+from .models import Order
 from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
@@ -19,3 +23,40 @@ def update_on_delete(sender, instance, **kwargs):
     Update order total on lineitem delete
     """
     instance.order.update_total()
+
+
+@receiver(post_save, sender=Order)
+def send_order_status_email(sender, instance, created, **kwargs):
+    if not created:
+        # Check that the status has changed to the desired value
+        if instance.status == 'shipped':
+            cust_email = instance.email
+            subject = render_to_string(
+                'checkout/confirmation_emails/shipped_order_confirmation_email_subject.txt',
+                {'order': instance})
+            body = render_to_string(
+                'checkout/confirmation_emails/shipped_order_confirmation_email_body.txt',
+                {'order': instance, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [cust_email]
+            )
+
+        if instance.status == 'completed':
+            cust_email = instance.email
+            subject = render_to_string(
+                'checkout/confirmation_emails/completed_order_confirmation_email_subject.txt',
+                {'order': instance})
+            body = render_to_string(
+                'checkout/confirmation_emails/completed_order_confirmation_email_body.txt',
+                {'order': instance, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [cust_email]
+            )
--- fithub/settings.py
+++ fithub/settings.py
@@ -122,6 +122,7 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'fithub@example.com'
 
 
 ACCOUNT_LOGIN_METHODS = {'email', 'username'}
@@ -213,7 +214,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#Stripe
+# Stripe
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
