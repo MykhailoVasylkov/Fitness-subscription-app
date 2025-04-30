0fe585a03d9ab217a4d896db050807c9a5d49daa
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-04-30 11:36:55 +0100

    Added plans page with plan cards rendering by categories

--- plans/views.py
+++ plans/views.py
@@ -4,12 +4,16 @@ from .models import SubscriptionPlan
 # Used code from Boutique Ado and Chat-GPT
 
 def all_plans(request):
-    """ A view to show all products, including sorting queries """
+    """ A view to show all plans """
 
     plans = SubscriptionPlan.objects.all()
+    nutrition_plans = SubscriptionPlan.objects.filter(category='nutrition')
+    exercises_plans = SubscriptionPlan.objects.filter(category='exercises')
 
     context = {
         'plans': plans,
+        'nutrition_plans': nutrition_plans,
+        'exercises_plans': exercises_plans,
     }
 
     return render(request, 'plans/plans.html', context)
