9d869be9d2ca2499a11dd5d47dfa2f175ee3b479
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-05-13 16:07:23 +0100

    Mount the card element for subscription payments in stripe elements script

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -29,28 +29,54 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var productCard = productElements.create('card', { style: style });
-productCard.mount('#product-card-element');
+// Mount the card element for product payments if it exists
+var productCardElement = document.getElementById('product-card-element');
+if (productCardElement) {
+    var productCard = productElements.create('card', { style: style });
+    productCard.mount('#product-card-element');
 
-// Mount the card element for subscription payments
-var subscriptionCard = subscriptionElements.create('card', { style: style });
-subscriptionCard.mount('#subscription-card-element');
+    // Handle realtime validation errors on the card element
+    productCard.addEventListener('change', function (event) {
+        var errorDiv = document.getElementById('product-card-errors');
+        if (event.error) {
+            var html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${event.error.message}</span>
+            `;
+            $(errorDiv).html(html);
+        } else {
+            errorDiv.textContent = '';
+        }
+    });
+}
 
-// Handle realtime validation errors on the card element
-card.addEventListener('change', function (event) {
-    var errorDiv = document.getElementById('card-errors');
-    if (event.error) {
-        var html = `
-            <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-            </span>
-            <span>${event.error.message}</span>
-        `;
-        $(errorDiv).html(html);
-    } else {
-        errorDiv.textContent = '';
-    }
-});
+// Mount the card element for subscription payments if it exists
+var subscriptionCardElement = document.getElementById('subscription-card-element');
+if (subscriptionCardElement) {
+    // Mount the card element for subscription payments
+    var subscriptionCard = subscriptionElements.create('card', { style: style });
+    subscriptionCard.mount('#subscription-card-element');
+
+    subscriptionCard.addEventListener('change', function (event) {
+        var errorDiv = document.getElementById('subscription-card-errors');
+        if (event.error) {
+            var html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${event.error.message}</span>
+            `;
+            $(errorDiv).html(html);
+        } else {
+            errorDiv.textContent = '';
+        }
+    });
+}
+
+
+
 
 // Handle form submissions
 ['product-payment-form', 'subscription-payment-form'].forEach(function(formId) {
@@ -58,6 +84,9 @@ card.addEventListener('change', function (event) {
     if (!form) return;
     form.addEventListener('submit', function(ev) {
         ev.preventDefault();
+
+        var isSubscription = formId === 'subscription-payment-form';
+        var card = isSubscription ? subscriptionCard : productCard;
         card.update({ 'disabled': true });
         $('#submit-button').attr('disabled', true);
         $(form).fadeToggle(100);
@@ -65,13 +94,21 @@ card.addEventListener('change', function (event) {
 
         var saveInfo = Boolean($('#id-save-info').attr('checked'));
         var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-        var isSubscription = formId === 'subscription-payment-form';
         var clientSecret = isSubscription ? subscriptionClientSecret : productClientSecret;
-        var postData = {
-            'csrfmiddlewaretoken': csrfToken,
-            'client_secret': clientSecret,
-            'save_info': saveInfo,
-        };
+
+        if (!isSubscription) {
+            var postData = {
+                'csrfmiddlewaretoken': csrfToken,
+                'product_client_secret': productClientSecret,
+                'save_info': saveInfo,
+            };
+        } else {
+            var postData = {
+                'csrfmiddlewaretoken': csrfToken,
+                'subscription_client_secret': subscriptionClientSecret,
+                'save_info': saveInfo,
+            };
+        }
         var url = '/checkout/cache_checkout_data/';
 
         // Prepare billing details
@@ -110,7 +147,7 @@ card.addEventListener('change', function (event) {
                 } : undefined,
             }).then(function(result) {
                 if (result.error) {
-                    var errorDiv = document.getElementById('card-errors');
+                    var errorDiv = document.getElementById(isSubscription ? 'subscription-card-errors' : 'product-card-errors');
                     var html = `
                         <span class="icon" role="alert">
                         <i class="fas fa-times"></i>
