2989a09d6a8c046d1dde5bbe8131b90c6d0d435c
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-05-19 09:44:59 +0100

    Added CommunityMessage model for collecting and rendering messages from websocket

---
+++ community/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import CommunityMessage
+
+# Create your views here.
+
+def community(request):
+     """ A view to return the community page """
+     
+     сommunity_messages = CommunityMessage.objects.order_by('-timestamp')[:20]
+     return render(request, 'community/community.html', {'сommunity_messages': сommunity_messages})
