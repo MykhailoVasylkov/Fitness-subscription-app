c5e9cfae0a17fc4278910321fed23aff7f10cc14
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-05-22 09:44:38 +0100

    Added terms of use and privacy policy pages

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('/terms', views.terms, name='terms'),
+    path('/privacy', views.privacy, name='privacy'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ def index(request):
      """ A view to return the index page """
      
      return render(request, 'home/index.html')
+
+def terms(request):
+     """ A view to return the terms of use page """
+     
+     return render(request, 'home/terms_of_use.html')
+
+def privacy(request):
+     """ A view to return the privacy policy page """
+     
+     return render(request, 'home/privacy_policy.html')
