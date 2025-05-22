1f1e3a0c927de7f9f08a97b7dfeefb5e4529c33f
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-05-22 11:18:44 +0100

    Added NewsletterSubscriber model and registered in admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import NewsletterSubscriber
 # Register your models here.
+
+@admin.register(NewsletterSubscriber)
+class NewsletterSubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscribed_at', 'is_active', 'unsubscribe_token')
+    list_filter = ('is_active',)
+    search_fields = ('email',)
+    readonly_fields = ('unsubscribe_token',)  # токен не редактируется вручную
