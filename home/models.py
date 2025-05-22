3ead61445ab878fb737cf3864e26e019d9574a3a
MykhailoVasylkov <vasilkoff.mihail@gmail.com>
2025-05-22 14:20:09 +0100

    Added confirm subscription and unsubscribe functionality

--- home/models.py
+++ home/models.py
@@ -6,6 +6,7 @@ class NewsletterSubscriber(models.Model):
     subscribed_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     unsubscribe_token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    confirmation_token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
 
     def __str__(self):
         return self.email
--- home/views.py
+++ home/views.py
@@ -1,41 +1,106 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+import uuid
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import get_object_or_404
+from django.conf import settings
+
 from .forms import NewsletterSignupForm
 from django.contrib import messages
 from .models import NewsletterSubscriber
 
-
-# Create your views here.
-
-from django.contrib import messages
-from .models import NewsletterSubscriber
-from .forms import NewsletterSignupForm
 
 def index(request):
-    """
-    A view to return the index page and collect subscription emails
-    """
-    if request.method == 'POST':
-        form = NewsletterSignupForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            try:
-                subscriber = NewsletterSubscriber.objects.get(email=email)
-                if subscriber.is_active:
-                    messages.info(request, 'You are already subscribed to our newsletter.')
-                else:
-                    subscriber.is_active = True
-                    subscriber.save()
-                    messages.success(request, 'You have been re-subscribed to the newsletter.')
-            except NewsletterSubscriber.DoesNotExist:
-                # New email, create a subscriber
-                NewsletterSubscriber.objects.create(email=email, is_active=True)
-                messages.success(request, 'Thank you for subscribing!')
+     """
+     A view to return the index page and collect subscription emails with confirmation token.
+     Used Chat-GPT
+     """
+     if request.method == 'POST':
+          form = NewsletterSignupForm(request.POST)
+          if form.is_valid():
+               email = form.cleaned_data['email']
+               try:
+                    subscriber = NewsletterSubscriber.objects.get(email=email)
+                    if subscriber.is_active:
+                         messages.info(request, 'You are already subscribed to our newsletter.')
+                         return redirect('home')
+                    else:
+                         # Inactive subscriber - need to send a new confirmation email
+                         subscriber.unsubscribe_token = uuid.uuid4()
+                         subscriber.confirmation_token = uuid.uuid4()
+                         subscriber.is_active = False
+                         subscriber.save()
+               except NewsletterSubscriber.DoesNotExist:
+                    # New subscriber
+                    subscriber = NewsletterSubscriber.objects.create(
+                    email=email,
+                    is_active=False,
+                    confirmation_token=uuid.uuid4()
+                    )
 
-            return redirect('home')
+               # We are sending a confirmation letter
+               current_site = get_current_site(request)
+               confirmation_link = request.build_absolute_uri(
+                    reverse('confirm_subscription', args=[str(subscriber.confirmation_token)])
+               )
+               subject = 'Confirm your subscription'
+               from_email = settings.DEFAULT_FROM_EMAIL
+               to = [subscriber.email]
+
+               context = {
+                    'confirmation_link': confirmation_link,
+                    'domain': current_site.domain,
+                    'subscriber': subscriber,
+               }
+
+               html_content = render_to_string('emails/confirm_email_subscription.html', context)
+               text_content = f'Please confirm your subscription by visiting: {confirmation_link}'
+
+               email_msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+               email_msg.attach_alternative(html_content, "text/html")
+               email_msg.send()
+
+               messages.success(request, 'Please check your email to confirm your subscription.')
+               return redirect('home')
+     else:
+          form = NewsletterSignupForm()
+
+     return render(request, 'home/index.html', {'form': form})
+
+
+def confirm_subscription(request, token):
+     try:
+          subscriber = NewsletterSubscriber.objects.get(confirmation_token=token)
+          subscriber.is_active = True
+          subscriber.save()
+     except NewsletterSubscriber.DoesNotExist:
+          subscriber = None
+
+     unsubscribe_link = request.build_absolute_uri(
+          reverse('unsubscribe', args=[str(subscriber.unsubscribe_token)])
+)
+
+     current_site = get_current_site(request)
+     context = {
+          'subscriber': subscriber,
+          'unsubscribe_link': unsubscribe_link,
+     }
+     return render(request, 'home/confirm_subscription.html', context)
+
+
+def unsubscribe(request, token):
+    subscriber = get_object_or_404(NewsletterSubscriber, unsubscribe_token=token)
+
+    if subscriber.is_active:
+        subscriber.is_active = False
+        subscriber.save()
+        messages.success(request, f'You have successfully unsubscribed from the newsletter.')
     else:
-        form = NewsletterSignupForm()
+        messages.info(request, 'You are already unsubscribed.')
 
-    return render(request, 'home/index.html', {'form': form})
+    return redirect('home')
 
 
 def terms(request):
